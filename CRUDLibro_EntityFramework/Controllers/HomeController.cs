b081ef5083ec4258e9b9a8f1807828238ba38486
Diego Fernando Bordon <bordond@hotmail.com>
2021-11-26 22:43:38 -0300

    Agregar archivos de proyecto.

---
+++ CRUDLibro_EntityFramework/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDLibro_EntityFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Este es el Segundo Parcial de la Materia Programacion Visual.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+    }
+}
---
+++ CRUDLibro_EntityFramework/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CRUDLibro_EntityFramework.MvcApplication" Language="C#" %>
